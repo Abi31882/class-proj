c5caddedd442d12e58b332f26db9ea8e6940590e
Abi chaudhary <abhinavchaudhary348@gmail.com>
2021-08-21 23:58:13 +0530

    debounce

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+import { CANCEL } from "redux-saga";
 
 export const BASE_URL = "https://api-dev.domecompass.com";
 
@@ -22,3 +23,13 @@ axios.interceptors.response.use(undefined, (error) => {
   }
   return Promise.reject(error);
 });
+
+export const get = <T>(url: string, config: AxiosRequestConfig) => {
+  console.log(CANCEL);
+
+  const source = axios.CancelToken.source();
+
+  const response = axios.get<T>(url, { ...config, cancelToken: source.token });
+  response[CANCEL] = source.cancel;
+  return response;
+};
--- src/sagas/groups.sagas.ts
+++ src/sagas/groups.sagas.ts
@@ -1,18 +1,33 @@
-import { takeEvery, call, put } from "@redux-saga/core/effects";
+import { takeLatest, delay, call, put } from "@redux-saga/core/effects";
 import { AnyAction } from "redux";
 import { GROUPS_QUERY_CHANGED } from "../actions/action.constants";
 import { QueryCompletedAction } from "../actions/groups.actions";
 import { fetchGroups as fetchGroupsApi } from "../api/groups";
 
+// export function* fetchGroups(action: AnyAction): Generator<any> {
+//   const groupRes: any = yield call(fetchGroupsApi, {
+//     query: action.payload,
+//     status: "all-groups",
+//   });
+
+//   yield put(QueryCompletedAction(action.payload, groupRes.data.data));
+// }
+
+// export function* watchGroupQueryChange() {
+//   yield debounce(1000, GROUPS_QUERY_CHANGED, fetchGroups);
+// }
+
 export function* fetchGroups(action: AnyAction): Generator<any> {
-  const output: any = yield call(fetchGroupsApi, {
+  yield delay(1000);
+
+  const groupRes: any = yield call(fetchGroupsApi, {
     query: action.payload,
     status: "all-groups",
   });
 
-  yield put(QueryCompletedAction(action.payload, output));
+  yield put(QueryCompletedAction(action.payload, groupRes.data.data));
 }
 
 export function* watchGroupQueryChange() {
-  yield takeEvery(GROUPS_QUERY_CHANGED, fetchGroups);
+  yield takeLatest(GROUPS_QUERY_CHANGED, fetchGroups);
 }
